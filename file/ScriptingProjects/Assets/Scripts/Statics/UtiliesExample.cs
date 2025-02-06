198b334f1ada11bb5ef23fcdd449420cdb353860
Steven Gomez <719513@student.dusd.net>
2025-02-06 15:26:12 -0800

    h

---
+++ file/ScriptingProjects/Assets/Scripts/Statics/UtiliesExample.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UtiliesExample : MonoBehaviour
+{
+    private void Start()
+    {
+        int x = Utilities.Add(5, 6);
+    }
+}
