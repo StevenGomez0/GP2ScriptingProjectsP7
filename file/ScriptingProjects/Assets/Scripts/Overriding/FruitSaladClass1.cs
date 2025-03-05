278357a95885134b7529bd8928cca803900e9692
Steven Gomez <719513@student.dusd.net>
2025-03-05 12:02:50 -0800

    Overriding challenge

---
+++ file/ScriptingProjects/Assets/Scripts/Overriding/FruitSaladClass1.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FruitSaladClass1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        AppleClass1 myApple = new AppleClass1();
+
+        myApple.SayHello();
+        myApple.Chop();
+
+        FruitClass1 myFruit = new AppleClass1();
+        myFruit.SayHello();
+        myFruit.Chop();
+    }
+}
